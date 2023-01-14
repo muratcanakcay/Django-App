3d38ccdc65525ae35be3070a789583e6b54406ed
Muratcan Akcay <muratcanakcay@gmail.com>
2023-01-15 00:50:29 +0100

    add user does not exist message

--- gossipzone/base/views.py
+++ gossipzone/base/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Zone, Topic
 from .forms import ZoneForm
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+
     context = {}
     return render(request, 'base/login_register.html', context)
 
