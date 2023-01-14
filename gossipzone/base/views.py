365116849cab56b6fea2d28208211b48dd93d6ba
Muratcan Akcay <muratcanakcay@gmail.com>
2023-01-14 11:58:07 +0100

    add navbar.html template

--- gossipzone/base/views.py
+++ gossipzone/base/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
 
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'home.html')
 
 def zone(request):
-    return HttpResponse('ZONE')
+    return render(request, 'zone.html')
