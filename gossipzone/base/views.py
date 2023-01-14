386b602f653e68ceba92523c59747f33b2039448
Muratcan Akcay <muratcanakcay@gmail.com>
2023-01-14 12:33:11 +0100

    add context for home

--- gossipzone/base/views.py
+++ gossipzone/base/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
-
+zones = [
+    {'id':1, 'name':'Nude photos of Britney Spears!'},
+    {'id':2, 'name':'Tina from the bookstore'},
+    {'id':3, 'name':'Chad has a child?'},
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'zones':zones}
+    return render(request, 'home.html', context)
 
 def zone(request):
     return render(request, 'zone.html')
