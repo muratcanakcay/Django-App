a07ae44e6a1cbaeaca8285f3c21c360a15cdabde
Muratcan Akcay <muratcanakcay@gmail.com>
2023-01-16 07:05:25 +0100

    fix user model, re-migrate db

--- gossipzone/base/models.py
+++ gossipzone/base/models.py
@@ -4,11 +4,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):    
     handle = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
-    bio = models.TextField(null=True)
+    bio = models.TextField(null=True, blank=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
 
-    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
 class Topic(models.Model):
