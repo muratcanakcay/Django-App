471d2a052acfe99d6e360e952056533f26ed8417
Muratcan Akcay <muratcanakcay@gmail.com>
2023-01-16 07:17:22 +0100

    Update script.js

--- gossipzone/static/js/script.js
+++ gossipzone/static/js/script.js
@@ -9,17 +9,6 @@ if (dropdownButton) {
   });
 }
 
-// Upload Image
-const photoInput = document.querySelector("#avatar");
-const photoPreview = document.querySelector("#preview-avatar");
-if (photoInput)
-  photoInput.onchange = () => {
-    const [file] = photoInput.files;
-    if (file) {
-      photoPreview.src = URL.createObjectURL(file);
-    }
-  };
-
 // Scroll to Bottom
 const conversationThread = document.querySelector(".zone__box");
 if (conversationThread) conversationThread.scrollTop = conversationThread.scrollHeight;
